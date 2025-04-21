02b8dac5064a3438102ea7b8fad640ebf830aa45
muthomi100 <techeasesolns@gmail.com>
2025-04-21 23:16:54 +0300

    Initial init.

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-blue-500 text-white">
+      <h1 className="text-5xl font-bold">⚽ Welcome to Razorbill!</h1>
+    </div>
+  );
+}
