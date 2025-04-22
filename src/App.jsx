d6025d18123b6a8592a23e9a63f49a9281d6c1bc
muthomi100 <techeasesolns@gmail.com>
2025-04-22 03:55:55 +0300

    Working to make header, footer and bg global, then work on straming.

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,15 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import HomePage from "./pages/Homepage";
+import Matches from "./pages/Matches"; // Import MatchesPage component
+import MatchDetails from "./pages/MatchDetails";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
       <Route index element={<HomePage />} />
+      <Route path="/matches" element={<Matches />} />
+      <Route path="/watch/:matchId" element={<MatchDetails />} />
       {/* Add more routes here later */}
     </>,
   ),
