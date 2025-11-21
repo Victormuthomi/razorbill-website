241e5b61d7d68b5134494cbdc60034b152451a11
Victormuthomi100 <victormuthomi.dev@gmail.com>
2025-11-21 14:29:23 +0300

    change urls to prod.

--- src/api.js
+++ src/api.js
@@ -2,13 +2,13 @@
 const BASE_URL = "https://razorbill-backend.onrender.com/api";
 
 //for local host
-export const BLOG_URL = "http://localhost:8080/blogs";
-export const AUTHOR_URL = "http://localhost:8080/authors";
-export const COMMENTS_URL = "http://localhost:8080/comments";
+//export const BLOG_URL = "http://localhost:8080/blogs";
+//export const AUTHOR_URL = "http://localhost:8080/authors";
+//export const COMMENTS_URL = "http://localhost:8080/comments";
 
 //for prod
-//export const BLOG_URL = "https://razorblog-backend.onrender.com/blogs";
-//export const AUTHOR_URL = "https://razorblog-backend.onrender.com/authors";
-//export const COMMENTS_URL = "https://razorblog-backend.onrender.com/comments";
+export const BLOG_URL = "https://razorblog-backend.onrender.com/blogs";
+export const AUTHOR_URL = "https://razorblog-backend.onrender.com/authors";
+export const COMMENTS_URL = "https://razorblog-backend.onrender.com/comments";
 
 export default BASE_URL;
